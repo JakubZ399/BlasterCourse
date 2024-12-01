fdc2b8f7bc14fec8f6de358e76cb8e07aaaf3ff3
kubaz <zurawikjakub@gmail.com>
2024-12-01 20:03:34 +0100

    Create Combat component class

---
+++ Source/BlasterCourse/BlasterComponents/CombatComponent.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CombatComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BLASTERCOURSE_API UCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UCombatComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+protected:
+	virtual void BeginPlay() override;
+
+private:	
+	
+};
